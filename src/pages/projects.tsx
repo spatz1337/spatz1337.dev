399d80c0361a8dc771e32640034ecd4df7c08878
spatz1337 <77550188+spatz1337@users.noreply.github.com>
2023-01-23 14:31:22 +0000

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,8 +6,8 @@ import { ProjectCard } from 'components/ProjectCard'
 const Projects = () => {
   return (
     <MainLayout
-      title="Projects"
-      description="A list of all the projects I've worked on or I'm currently working on."
+      title="a.k.a eren sezen"
+      description="Deutscher Cybersicherheitsexperte"
     >
       <div className="grid gap-6">
         {ProjectData.map((project) => (
