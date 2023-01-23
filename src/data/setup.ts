0a4d6d10e4d79d77cf6c84759f10ad05afdc165f
spatz1337 <77550188+spatz1337@users.noreply.github.com>
2023-01-23 14:27:10 +0000

    Update setup.ts

--- src/data/setup.ts
+++ src/data/setup.ts
@@ -6,73 +6,13 @@ export interface Component {
 
 export const ComponentData: Component[] = [
   {
-    type: 'Case',
-    name: 'NZXT H510',
-    href: 'https://nzxt.com/product/h510',
+    type: '1337 Nedir?',
+    name: '1337 Hakkında Bilgiler',
+    href: 'https://tr.wikipedia.org/wiki/Leet',
   },
   {
-    type: 'Motherboard',
-    name: 'AORUS Z390 PRO WIFI',
-    href: 'https://www.gigabyte.com/Motherboard/Z390-AORUS-PRO-WIFI-rev-10#kf',
-  },
-  {
-    type: 'PSU',
-    name: 'EVGA Supernova 80+ Platinum 650W',
-    href: 'https://www.amazon.com/EVGA-Supernova-Platinum-Warranty-220-P2-0650-X1/dp/B010HWDPKW/ref=sr_1_7?crid=2QY46B6NCZYGS&keywords=evga%2Bpsu%2B650w%2Bfully%2Bmodular&qid=1654121572&s=electronics&sprefix=evga%2Bpsu%2B650w%2Bfull%2Bmodular%2Celectronics%2C54&sr=1-7&th=1',
-  },
-  {
-    type: 'GPU',
-    name: 'EVGA NVIDIA Geforce GTX 1060 3GB',
-    href: 'https://www.amazon.com/EVGA-GeForce-Support-Graphics-03G-P4-6160-KR/dp/B01KUADE3O?th=1',
-  },
-  {
-    type: 'CPU',
-    name: 'Intel Core i7-9700k',
-    href: 'https://www.intel.com/content/www/us/en/products/sku/186604/intel-core-i79700k-processor-12m-cache-up-to-4-90-ghz/specifications.html',
-  },
-  {
-    type: 'CPU Cooler',
-    name: 'Cooler Master I71C Intel LGA1151',
-    href: 'https://www.amazon.com/Cooler-Master-RR-I71C-20PC-R1-Anodized-Aluminum/dp/B07H1VZ11F/ref=sr_1_9?crid=2UM04U65W7UCY&keywords=cooler+master+cpu+cooler&qid=1654121356&s=electronics&sprefix=cooler+master+cpu+cooler%2Celectronics%2C72&sr=1-9',
-  },
-  {
-    type: 'Memory',
-    name: 'G-SKILL Ripjaws DDR4-2133 32GB (2 x 16GB)',
-    href: 'https://www.amazon.com/G-SKILL-Ripjaws-288-Pin-Desktop-F4-2133C15D-32GVR/dp/B01AO6RUIE',
-  },
-  {
-    type: 'Storage',
-    name: 'WD Blue NVMe M.2 SSD 1TB',
-    href: 'https://www.newegg.com/western-digital-blue-sn550-nvme-1tb/p/N82E16820250135',
-  },
-  {
-    type: 'Storage 2',
-    name: 'Samsung SSD 860 EVO SSD 1TB',
-    href: 'https://www.amazon.com/gp/product/B078DPCY3T/ref=ppx_yo_dt_b_asin_title_o01_s01?ie=UTF8&psc=1',
-  },
-  {
-    type: 'Storage 3',
-    name: 'Samsung SSD 860 EVO SSD 500GB',
-    href: 'https://www.amazon.com/SAMSUNG-500GB-Internal-MZ-77E500B-AM/dp/B08QBMD6P4/ref=sr_1_1?crid=2ESPEADXH3TKQ&keywords=Samsung+SSD+860+EVO+500gb&qid=1654119480&s=electronics&sprefix=samsung+ssd+860+evo+500gb%2Celectronics%2C66&sr=1-1',
-  },
-  {
-    type: 'Storage 4',
-    name: 'WD 4TB Black HDD',
-    href: 'https://www.amazon.com/Black-Passport-Portable-External-Drive/dp/B01LQQH86A/ref=sr_1_5?crid=2U7J1DYAGV00M&keywords=wd+my+passport+4tb&qid=1654119502&s=electronics&sprefix=wd+my+passport+4tb%2Celectronics%2C153&sr=1-5',
-  },
-  {
-    type: 'Monitor',
-    name: 'LG 240hz 1ms 1080p IPS',
-    href: 'https://www.amazon.com/LG-Electronics-27GN750-B-Compatibility-Adjustable/dp/B0841787BZ/ref=sr_1_3?crid=31WAFXJJKLTIB&keywords=lg+240hz+1ms&qid=1654121687&s=electronics&sprefix=lg+240hz+1ms%2Celectronics%2C173&sr=1-3',
-  },
-  {
-    type: 'Keyboard',
-    name: 'Logitech G Pro',
-    href: 'https://www.amazon.com/Logitech-Mechanical-Tenkeyless-Detachable-LIGHTSYNC/dp/B07QQB9VCV/ref=sr_1_3?keywords=logitech+g+pro+keyboard&qid=1654121790&sprefix=logitech+g+pro+key%2Caps%2C220&sr=8-3',
-  },
-  {
-    type: 'Mouse',
-    name: 'Glorious Model O Wireless',
-    href: 'https://www.amazon.com/dp/B098RDFP3J/ref=twister_B098RCVV3H?_encoding=UTF8&th=1',
+    type: '1337 Alfabesi Nedir?',
+    name: '1337 Alfabesi Nedir Ve Nasıl Öğrenebilirim',
+    href: 'https://cdn.discordapp.com/attachments/1056556915934638141/1067076080298627072/spatz1337.png',
   },
 ]
