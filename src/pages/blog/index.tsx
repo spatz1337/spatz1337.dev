7df608d8b042ac29fb0845e1494e6d4dfc80679f
spatz1337 <77550188+spatz1337@users.noreply.github.com>
2023-03-11 18:04:29 +0300

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -8,7 +8,7 @@ const Blog = ({ allPostsData }: InferGetStaticPropsType<typeof getStaticProps>) 
   return (
     <MainLayout
       title="Blog"
-      description="contact for spatz#1337."
+      description="contact for @erennszn"
     >
       <div className="mt-8 grid gap-8">
         {allPostsData.map(({ slug, date, title, excerpt }) => {
